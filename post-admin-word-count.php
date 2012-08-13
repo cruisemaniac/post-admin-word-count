52e1e475f43ebf27f4d69da7cd0ec6fb739d9ca1
Jon Bishop <jonbish@gmail.com>
2012-08-13 17:21:01 -0400

    Trying to optimize

--- post-admin-word-count.php
+++ post-admin-word-count.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Plugin Name: Post Admin Word Count
   Plugin URI: http://www.jonbishop.com/downloads/wordpress-plugins/post-word-count/
@@ -8,63 +9,86 @@
   Author URI: http://www.jonbishop.com
   License: GPL2
  */
-class PostAdminWordCount{
-	function init() {
-		if(is_admin()){
-			add_filter('manage_edit-post_sortable_columns', array(&$this, 'pwc_column_register_sortable'));
-			add_filter('posts_orderby', array(&$this, 'pwc_column_orderby'), 10, 2);
-			add_filter("manage_posts_columns", array(&$this, "pwc_columns"));
-			add_action("manage_posts_custom_column", array(&$this, "pwc_column"));
-		}
-	}
 
-	//=============================================
-	// Add new columns to action post type
-	//=============================================
-	function pwc_columns($columns) {
-		$columns["post_word_count"] = "Word Count";
-		return $columns;
-	}
-	
-	//=============================================
-	// Add data to new columns of action post type
-	//=============================================
-	function pwc_column($column){
-		global $post;
-		if ("post_word_count" == $column) {
-			$saved_word_count = get_post_meta($post->ID, '_post_word_count', true);
-			$word_count = str_word_count($post->post_content);
-			if($saved_word_count != $word_count){
-				update_post_meta($post->ID, '_post_word_count', $word_count, $saved_word_count);
-			}
-			echo $word_count;
-		}
-	}
-	
-	//=============================================
-	// Queries to run when sorting
-	// new columns of action post type
-	//=============================================
-	function pwc_column_orderby($orderby, $wp_query) {
-		global $wpdb;
-	 
-		$wp_query->query = wp_parse_args($wp_query->query);
+class PostAdminWordCount {
 
-		if ( 'post_word_count' == @$wp_query->query['orderby'] )
-			$orderby = "(SELECT CAST(meta_value as decimal) FROM $wpdb->postmeta WHERE post_id = $wpdb->posts.ID AND meta_key = '_post_word_count') " . $wp_query->get('order');
+    function init() {
+        if (is_admin()) {
+            add_filter('manage_edit-post_sortable_columns', array(&$this, 'pwc_column_register_sortable'));
+            add_filter('posts_orderby', array(&$this, 'pwc_column_orderby'), 10, 2);
+            add_filter("manage_posts_columns", array(&$this, "pwc_columns"));
+            add_action("manage_posts_custom_column", array(&$this, "pwc_column"));
+            add_action("admin_footer-edit.php",array(&$this, "pwc_update_date"));
+            add_action("admin_head-edit.php",array(&$this, "pwc_get_date"));
+            
+        }
+    }
 
-		return $orderby;
-				
-	}
-	
-	//=============================================
-	// Make new columns to action post type sortable
-	//=============================================
-	function pwc_column_register_sortable($columns) {
-		$columns['post_word_count'] = 'post_word_count';
-		return $columns;
-	}
+    //=============================================
+    // Add new columns to action post type
+    //=============================================
+    function pwc_columns($columns) {
+        $columns["post_word_count"] = "Word Count";
+        return $columns;
+    }
+
+    //=============================================
+    // Add data to new columns of action post type
+    //=============================================
+    function pwc_column($column) {
+        global $post, $pwc_last;
+        if ("post_word_count" == $column) {
+            // Grab a fresh word count
+            $word_count = str_word_count($post->post_content);
+
+            // If post has been updated since last check
+            if ($post->post_modified > $pwc_last) {
+                $saved_word_count = get_post_meta($post->ID, '_post_word_count', true);
+                if ($saved_word_count != $word_count || $saved_word_count == "") {
+                    update_post_meta($post->ID, '_post_word_count', $word_count, $saved_word_count);
+                }
+            }
+            echo $word_count;
+        }
+    }
+
+    //=============================================
+    // Queries to run when sorting
+    // new columns of action post type
+    //=============================================
+    function pwc_column_orderby($orderby, $wp_query) {
+        global $wpdb;
+
+        $wp_query->query = wp_parse_args($wp_query->query);
+
+        if ('post_word_count' == @$wp_query->query['orderby'])
+            $orderby = "(SELECT CAST(meta_value as decimal) FROM $wpdb->postmeta WHERE post_id = $wpdb->posts.ID AND meta_key = '_post_word_count') " . $wp_query->get('order');
+
+        return $orderby;
+    }
+
+    //=============================================
+    // Make new columns to action post type sortable
+    //=============================================
+    function pwc_column_register_sortable($columns) {
+        $columns['post_word_count'] = 'post_word_count';
+        return $columns;
+    }
+    
+    function pwc_get_date(){
+        global $pwc_last;
+        // Check last updated
+        if (!isset($pwc_last) || $pwc_last == '')
+            $pwc_last = get_option('pwc_last_checked');
+    }
+    
+    function pwc_update_date(){
+        $current_date = current_time('mysql');
+        update_option('pwc_last_checked', $current_date);
+    }
+
 }
+
 $postAdminWordCount = new PostAdminWordCount();
 $postAdminWordCount->init();
 ?>
