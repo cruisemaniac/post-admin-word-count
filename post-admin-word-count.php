918fa9d53b41f291e9a567a2d60987b87f84d541
Jon Bishop <jonbish@gmail.com>
2012-08-14 00:06:10 -0400

    New sort code

--- post-admin-word-count.php
+++ post-admin-word-count.php
@@ -4,7 +4,7 @@
   Plugin Name: Post Admin Word Count
   Plugin URI: http://www.jonbishop.com/downloads/wordpress-plugins/post-word-count/
   Description: Adds a sortable column to the admin's post manager, displaying the word count for each post.
-  Version: 1.1
+  Version: 1.2
   Author: Jon Bishop
   Author URI: http://www.jonbishop.com
   License: GPL2
@@ -70,31 +70,42 @@ class PostAdminWordCount {
         // Check last updated
         $pwc_last = get_option('pwc_last_checked');
 
+        // Check to make sure we have a post and post type
 		if ( $post && $post->post_type ){
+			
+			// Grab all posts with post type
 			$args = array(
 				'post_type' => $post->post_type,
 				'posts_per_page' => -1
 				);
 
+			// Grab the posts
 			$post_list = new WP_Query($args);
 			if ( $post_list->have_posts() ) : while ( $post_list->have_posts() ) : $post_list->the_post(); 
+				
 				// Grab a fresh word count
 	            $word_count = str_word_count($post->post_content);
 
 	            // If post has been updated since last check
 	            if ($post->post_modified > $pwc_last || $pwc_last == "") {
+	            	// Grab word count from post meta
 	                $saved_word_count = get_post_meta($post->ID, '_post_word_count', true);
+	                // Check if new wordcount is different than old word count
 	                if ($saved_word_count != $word_count || $saved_word_count == "") {
+	                	// Update word count in post meta
 	                    update_post_meta($post->ID, '_post_word_count', $word_count, $saved_word_count);
 	                }
 	            }
 			endwhile; 
 			endif;
+
+			// Let WordPress do it's thing
 			wp_reset_query();
 		}
     }
     
     function pwc_update_date(){
+    	// Save the last time this page was generated
         $current_date = current_time('mysql');
         update_option('pwc_last_checked', $current_date);
     }
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.jonbishop.com/donate/
 Tags: wordcount, word count, words, count, column, admin, sortable, posts
 Requires at least: 3.0
 Tested up to: 3.4.1
-Stable tag: 1.1
+Stable tag: 1.2
 
 Adds a sortable column to the admin's post manager, displaying the word count for each post.
 
@@ -12,12 +12,22 @@ Adds a sortable column to the admin's post manager, displaying the word count fo
 
 Post Word Count adds a sortable column to the admin's post manager, displaying the word count for each post.
 
+I often create new drafts in WordPress every time I get an idea to blog. The problem was I had no easy way to see which drafts I had already started working on. This plugin allows me to sort all of my posts, including my drafts, by the posts word count.
+
 == Installation ==
 
 1. Upload the 'post-word-count' folder to the '/wp-content/plugins/' directory
 1. Activate the plugin through the 'Plugins' menu in WordPresss
 
+== Screenshots ==
+
+1. Word count on post list
+
 == Changelog ==
+
+= 1.2 =
+* Process post meta before display
+* Upload screenshot
 
 = 1.1 =
 * Fixed sorting
